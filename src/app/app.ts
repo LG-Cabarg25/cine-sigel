1296cf6f5a8a5a08a7d832136e0aa6dceff2e0df
Luis Gustavo Cabrera Argueta <lgcabarg25@gmail.com>
2025-08-11 09:33:27 -0600

    refactor: remove placeholder files and update app component template

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `
+  <h1>Hola Mundo</h1>
+  <router-outlet/>
+  `,
+
 })
 export class App {
   protected readonly title = signal('cine-sigel');
