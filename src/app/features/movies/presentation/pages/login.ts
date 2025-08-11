3a68c7d4a1f7c69ab23a9cfd09e360dfa509af7b
Luis Gustavo Cabrera Argueta <lgcabarg25@gmail.com>
2025-08-11 10:22:40 -0600

    feat: :art: add login page and configure routing

---
+++ src/app/features/movies/presentation/pages/login.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  template: `
+
+  <h2>este es el login</h2>
+  `,
+
+})
+export class LoginPage {
+
+}
